cecedda48205c4037b83ce60ea0e074ab8c4066d
Егор <egpol2004@yandex.ru>
2020-02-19 18:12:45 +0300

    fun get_coord and open_map_image

--- MHF.py
+++ MHF.py
@@ -44,10 +44,11 @@ def open_map_imge(coord):
         }
         map_api_server = "http://static-maps.yandex.ru/1.x/"
         response = requests.get(map_api_server, params=map_params)
-
-        Image.open(BytesIO(
-            response.content)).show()
-        return None
+        map_file = "map.png"
+        with open(map_file, "wb") as file:
+            file.write(response.content)
+        return map_file
     except Exception:
         print("ошибка")
         return None
+
